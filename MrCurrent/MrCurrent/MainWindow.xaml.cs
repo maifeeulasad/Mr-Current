0d5d7c8c36fbcd2883a5c9338cb4cc6ed3d0efb6
maifeeulasad <maifeeulasad@gmail.com>
2020-08-24 13:49:43 +0600

    battery status change event

--- MrCurrent/MrCurrent/MainWindow.xaml.cs
+++ MrCurrent/MrCurrent/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+﻿using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Forms;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Controls;
 
 namespace MrCurrent
 {
@@ -20,9 +11,65 @@ namespace MrCurrent
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static BatteryChargeStatus batteryChargeStatus 
+            = SystemInformation.PowerStatus.BatteryChargeStatus;
+        private static Grid gridMain = null;
+        private static SolidColorBrush lowPolyGreen 
+            = new SolidColorBrush(Color.FromArgb(255, (byte)99, (byte)177, (byte)50));
+        private static SolidColorBrush lowPolyRed
+            = new SolidColorBrush(Color.FromArgb(255, (byte)173, (byte)19, (byte)50));
+        private static SolidColorBrush lowPolyYellow
+            = new SolidColorBrush(Color.FromArgb(255, (byte)200, (byte)212, (byte)70));
+
         public MainWindow()
         {
             InitializeComponent();
+            SystemEvents.PowerModeChanged 
+                += new PowerModeChangedEventHandler(PowerModeChangeEventHandler);
+            gridMain = grid_main;
         }
+
+        static void PowerModeChangeEventHandler(
+            object sender, 
+            PowerModeChangedEventArgs e)
+        {
+            PowerStatus ps = SystemInformation.PowerStatus;
+            if (e.Mode == PowerModes.StatusChange)
+            {
+                batteryChargeStatus = ps.BatteryChargeStatus;
+                ChangeColor();
+            }
+        }
+
+        static void ChangeColor()
+        {
+            if (gridMain == null)
+                return;
+            switch(batteryChargeStatus)
+            {
+                case (BatteryChargeStatus.Critical):
+                    gridMain.Background = lowPolyRed;
+                    break;
+                case (BatteryChargeStatus.Low):
+                    gridMain.Background = lowPolyRed;
+                    break;
+                case (BatteryChargeStatus.NoSystemBattery):
+                    gridMain.Background = lowPolyRed;
+                    break;
+                case (BatteryChargeStatus.Unknown):
+                    gridMain.Background = lowPolyRed;
+                    break;
+                case (BatteryChargeStatus.Charging):
+                    gridMain.Background = lowPolyGreen;
+                    break;
+                case (BatteryChargeStatus.High):
+                    gridMain.Background = lowPolyGreen;
+                    break;
+                default:
+                    gridMain.Background = lowPolyYellow;
+                    break;
+            }
+        }
+
     }
 }
